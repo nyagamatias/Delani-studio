772af20a8ca28f83f1dfb42502e4b9ddb273a470
nyagamatias <nyagamatias@gmail.com>
2021-06-21 16:34:52 +0300

    add final changes

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
-$(document).ready(function (){
-    $("#img1"). click(function (){
+$(document).ready(function () {
+    $("#image1").click(function () {
         $("#p1").toggle();
         $("#image1").toggle();
     })
     $("#p1").click(function () {
         $("#p1").toggle();
         $("#image1").toggle();
-      });
-      $("#image2").click(function () {
+    });
+    $("#image2").click(function () {
         $("#p2").toggle();
         $("#image2").toggle();
-      });
-      $("#p2").click(function () {
+    });
+    $("#p2").click(function () {
         $("#p2").toggle();
         $("#image2").toggle();
-      });
-      $("#image3").click(function () {
+    });
+    $("#image3").click(function () {
         $("#p3").toggle();
         $("#image3").toggle();
-      });
-      $("#p3").click(function () {
+    });
+    $("#p3").click(function () {
         $("#p3").toggle();
         $("#image3").toggle();
-      });
-  });
- 
+    });
+});
+
