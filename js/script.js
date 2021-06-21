78646f75fb4cf2cb843a239241338e3fa93b3281
nyagamatias <nyagamatias@gmail.com>
2021-06-21 15:46:08 +0300

    add youtube icon

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function (){
+    $("#img1"). click(function (){
+        $("#p1").toggle();
+        $("#image1").toggle();
+    })
+    $("#p1").click(function () {
+        $("#p1").toggle();
+        $("#image1").toggle();
+      });
+      $("#image2").click(function () {
+        $("#p2").toggle();
+        $("#image2").toggle();
+      });
+      $("#p2").click(function () {
+        $("#p2").toggle();
+        $("#image2").toggle();
+      });
+      $("#image3").click(function () {
+        $("#p3").toggle();
+        $("#image3").toggle();
+      });
+      $("#p3").click(function () {
+        $("#p3").toggle();
+        $("#image3").toggle();
+      });
+  });
+ 
